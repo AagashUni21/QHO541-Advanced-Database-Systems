9605c4f8b68e5a56f7e04b2c9c125018105f0deb
Aagash <1rasaa48@solent.ac.uk>
2022-06-02 11:18:04 +0100

    setting up modules and shortcuts

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const bodyparser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -8,6 +10,14 @@ dotenv.config({path:'config.env'})
 const PORT =process.env.PORT||8080
 
 app.use(morgan('tiny'));
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+
+app.use('/css',express.static(path.resolve(__dirname, 'assets/css')));
+app.use('/images',express.static(path.resolve(__dirname, 'assets/images')));
+app.use('/js',express.static(path.resolve(__dirname, 'assets/js')));
 
 app.get('/',(req, res)=>{
     res.send("Application");
