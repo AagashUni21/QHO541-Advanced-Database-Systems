a41dc0c98b96a7b064386bfd6d152e354451cc5f
Aagash <1rasaa48@solent.ac.uk>
2022-06-06 20:05:57 +0100

    creating movie in db

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,27 @@
 var Advancedb=require('../model/model');
 
 exports.create=(req,res)=>{
+    if(!req.body){
+        res.status(400).send({message:"Content can not be empty!"});
+        return;
+    }
 
+    const movie=new Advancedb({
+        Movie_Title:req.body.Movie_Title,
+        Release_Date:req.body.Release_Date,
+        Box_Office:req.body.Box_Office
+    })
+
+    movie
+    .save(movie)
+    .then(data=>{
+        res.send(data)
+    })
+    .catch(err=>{
+        res.status(500).send({
+            message:err.message||"Some error has occured while creating a create operation"
+        });
+    });
 }
 
 exports.find=(req,res)=>{
